e033f98cabc24e847f3ede1e72030081ca51d21d
Julen Diaz <hello@julentech.com>
2022-06-20 16:59:06 +0200

    Init project

---
+++ src/main/java/com/blog/julentech/JulentechApplication.java
@@ -0,0 +1,13 @@
+package com.blog.julentech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulentechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JulentechApplication.class, args);
+    }
+
+}
